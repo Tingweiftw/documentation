301e251d01becc36f60eae499f82cc1554ee201b
thefiend <kam_ming_feng@tech.gov.sg>
2022-09-16 17:22:55 +0800

    Fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/introduction/GettingStarted">
             Start Tutorial - 4 hours ⏱️
           </Link>
         </div>
