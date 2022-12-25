801da1b731929c1f7e87ce7c4fb34cd772f6c5be
thefiend <kam_ming_feng@tech.gov.sg>
2022-12-25 13:58:07 +0800

    Update sitemap and alts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       navbar: {
         title: 'Engineering Bootcamp',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Learn Web Development',
           src: 'img/logo.svg',
         },
         items: [
